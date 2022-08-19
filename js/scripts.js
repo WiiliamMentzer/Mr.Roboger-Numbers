7a4f8e362de308f7ff0e29ccccf19c16b67f246b
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-19 12:03:05 -0700

    WIP number processing still broken.

--- js/scripts.js
+++ js/scripts.js
@@ -16,23 +16,26 @@ function robogerProcess() {
 
   processArray.forEach(function(element) {
     console.log(element);
-    for (let index = element.length -1; index >= 0; index--) {
+    for (let index = element.length; index >= 0; index--) {
       console.log(element[index]);
       if (element[index] === "3") {
         console.log("3 found");
-        processArray.splice(element, 1, "");
+        processArray.join();
+        processArray.splice(element, 1);
         outputArray.push("Won't you be my neighbor?")
       } else if (element[index] === "2") {
         console.log("2 found");
-        processArray.splice(element, 1, "");
+        processArray.join();
+        processArray.splice(element, 1);
         outputArray.push("Boop!")
       } else if (element[index] === "1") {
         console.log("1 found");
-        processArray.splice(element, 1, "");
+        processArray.join();
+        processArray.splice(element, 1);
         outputArray.push("Beep!")
       } else {
         console.log("nothing to replace here sir");
-        outputArray.push(element.toString());
+        outputArray.push(element[index].toString());
       }
     }
   });
