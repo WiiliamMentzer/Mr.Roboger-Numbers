ee659f51b8bef5323228f895b04b9615ffa8db3a
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-19 09:53:18 -0700

    updated processArray to parse through the array info to detect 1, 2 , or 3

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ console.log(13)
 Test: "It should return the processing of the inputArray"
 Code: robogerProcess();
 Expected Output: "13, 14, 15, 16, 17" [ [1,3], [1,4], [1,5], [1,6], [1,7] ]
+
+
+Test: "It should return each array of  processArray"
+Code: robogerProcess();
+userInput = "13, 14, 15, 16, 17";
+Expected Output: [1,3], [1,4], [1,5], [1,6], [1,7] 
+
+Test: "It should repeat "* found" if 1, 2, or 3 are found."
+Code: robogerProcess();
+userInput = "13, 55, 66";
+Expected Output: [1,3], [5,5], [6,6] 
+"1 found"
+"3 found"
+"nothing to replace here sir"
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let inputArray = [];
 let processArray = [];
 let outputArray = [];
-let userInput = "13 14 15 16 17";
+let userInput = "13 24 34 55 78";
 
 function robogerProcess() {
   inputArray = userInput.split(" ");
@@ -11,13 +11,21 @@ function robogerProcess() {
     processArray.push((element).split(""));
   });
 
-  console.log(processArray);
-
-  for (let index = 0; index < processArray.length; index +=1) {
-    console.log(index);
-  }
-  console.log(processArray);
-
+  processArray.forEach(function(element) {
+    console.log(element);
+    for (let index = 0; index < element.length; index +=1) {
+      console.log(element[index]);
+      if (element[index] === 3) {
+        console.log("3 found");
+      } else if (element[index] === 2) {
+        console.log("2 found");
+      } else if (element[index] === 1) {
+        console.log("1 found");
+      } else {
+        console.log("nothing to replace here sir");
+      }
+    }
+  });
 
   // for (let index = 0; index < processArray.length; index += 1) {
   //   console.log(index);
