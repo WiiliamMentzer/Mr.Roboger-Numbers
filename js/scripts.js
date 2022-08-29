4c6ee2035b28b932d139d905ec1a81837debc694
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-28 23:56:42 -0700

    word processing finished, now beginning dev on HTML implementation

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,38 @@ function robogerProcess() {
   let outputArray = [];
   let userInput = parseInt(document.getElementById("userInput").value);
 
+  console.log(userInput);
 
   for (index = 0; index <= userInput; index += 1) {
-    let inputArray = [];
-    inputArray.push(index.toString().split(""));
-    inputArray.forEach(function(element) {
-      if (inputArray.includes(3)) {
-        console.log("3 found");
-        outputArray.push("Won't you be my neighbor?");
-      } else if (inputArray.includes(2)) {
-        console.log("2 found");
-        outputArray.push("Boop!");
-      } else if (inputArray.includes(1)) {
-        console.log("1 found");
-        outputArray.push("Beep!");
-      } else {
-        console.log(element);
-        // console.log("nothing to replace here sir");
-      }
-    });
-  }
-}
-  // console.log(inputArray);
-  // inputArray.forEach(function*(element) {
-  //   inputArray.includes
-  // }
+    //let inputArray = [];
+    let inputArray = index.toString().split("");
 
-//   inputArray.forEach(function(element) {
-//     processArray.push((element).split(""));
-//   });
+    //console.log('temp values')
+    //console.log(temp);
+    //console.log(temp.includes('1'));
+
+    //console.log('array values')
+    //inputArray.push(temp);
+    // console.log(inputArray);
+    // console.log(inputArray.includes('1'));
+    if (inputArray.includes('3')) {
+      console.log("3 found");
+      outputArray.push("Won't you be my neighbor?");
+    } else if (inputArray.includes('2')) {
+      console.log("2 found");
+      outputArray.push("Boop!");
+    } else if (inputArray.includes('1')) {
+      console.log("1 found");
+      outputArray.push("Beep!");
+    } else {
+      console.log("failed");
+      outputArray.push(inputArray.join(""));
+      
+      // console.log("nothing to replace here sir");
+    }
+    console.log(outputArray);
+  }
+};
 
 //   processArray.forEach(function(element) {
 //     for (let index = element.length - 1; index >= 0; index--) {
