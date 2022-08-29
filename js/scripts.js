c9bc00f002916f9ee5a25c2a0fa875b9235b18c8
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-28 22:55:33 -0700

    reworking word processor once more

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,62 @@
 function robogerProcess() {
-  let inputArray = [];
-  let processArray = [];
   let outputArray = [];
   let userInput = parseInt(document.getElementById("userInput").value);
 
 
   for (index = 0; index <= userInput; index += 1) {
-    inputArray.push(index.toString());
-  }
-
-  console.log(inputArray);
-
-  inputArray.forEach(function(element) {
-    processArray.push((element).split(""));
-  });
-
-  processArray.forEach(function(element) {
-    for (let index = element.length - 1; index >= 0; index--) {
-      if (element[index] === "3") {
+    let inputArray = [];
+    inputArray.push(index.toString().split(""));
+    inputArray.forEach(function(element) {
+      if (inputArray.includes(3)) {
         console.log("3 found");
         outputArray.push("Won't you be my neighbor?");
-        element.concat("");
-        console.log(element);
-      } else if (element[index] === "2") {
+      } else if (inputArray.includes(2)) {
         console.log("2 found");
         outputArray.push("Boop!");
-      } else if (element[index] === "1") {
+      } else if (inputArray.includes(1)) {
         console.log("1 found");
         outputArray.push("Beep!");
       } else {
         console.log(element);
-        console.log("nothing to replace here sir");
-        outputArray.push(element.join());
+        // console.log("nothing to replace here sir");
       }
-    };
-  });
-
-  console.log(processArray);
-  console.log(outputArray);
-
+    });
+  }
 }
+  // console.log(inputArray);
+  // inputArray.forEach(function*(element) {
+  //   inputArray.includes
+  // }
+
+//   inputArray.forEach(function(element) {
+//     processArray.push((element).split(""));
+//   });
+
+//   processArray.forEach(function(element) {
+//     for (let index = element.length - 1; index >= 0; index--) {
+      // if (element[index] === "3") {
+      //   console.log("3 found");
+      //   outputArray.push("Won't you be my neighbor?");
+      //   element.concat("");
+      //   console.log(element);
+      // } else if (element[index] === "2") {
+      //   console.log("2 found");
+      //   outputArray.push("Boop!");
+      // } else if (element[index] === "1") {
+      //   console.log("1 found");
+      //   outputArray.push("Beep!");
+      // } else {
+      //   console.log(element);
+      //   console.log("nothing to replace here sir");
+      //   outputArray.push(element.join());
+      // }
+//     };
+//   });
+
+//   console.log(processArray);
+//   console.log(outputArray);
+
+// }
 
 window.addEventListener("load", function() {
   const form = document.getElementById("RobogerInput");
