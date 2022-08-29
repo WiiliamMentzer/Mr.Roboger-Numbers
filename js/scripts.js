0d97b188e677a570639f64ac280b9f66ce27bda9
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-29 05:03:28 -0700

    WIP css touchup for website, finished word processor with NaN reader. Finished displaying of word processor

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,8 @@ function robogerProcess() {
   console.log(userInput);
 
   for (index = 0; index <= userInput; index += 1) {
-    //let inputArray = [];
     let inputArray = index.toString().split("");
-
-    //console.log('temp values')
-    //console.log(temp);
-    //console.log(temp.includes('1'));
-
-    //console.log('array values')
-    //inputArray.push(temp);
-    // console.log(inputArray);
-    // console.log(inputArray.includes('1'));
+  
     if (inputArray.includes('3')) {
       console.log("3 found");
       outputArray.push("Won't you be my neighbor?");
@@ -28,38 +19,16 @@ function robogerProcess() {
     } else {
       console.log("failed");
       outputArray.push(inputArray.join(""));
-      
-      // console.log("nothing to replace here sir");
     }
     console.log(outputArray);
   }
+  
+  if (isNaN(userInput) || userInput === undefined) {
+    document.querySelector("p#ArrayDisplay").innerHTML = "You have not entered a correct value into the field, please try again.";
+  } else {
+    document.querySelector("p#ArrayDisplay").innerHTML = outputArray.join(", ");
+  }
 };
-
-//   processArray.forEach(function(element) {
-//     for (let index = element.length - 1; index >= 0; index--) {
-      // if (element[index] === "3") {
-      //   console.log("3 found");
-      //   outputArray.push("Won't you be my neighbor?");
-      //   element.concat("");
-      //   console.log(element);
-      // } else if (element[index] === "2") {
-      //   console.log("2 found");
-      //   outputArray.push("Boop!");
-      // } else if (element[index] === "1") {
-      //   console.log("1 found");
-      //   outputArray.push("Beep!");
-      // } else {
-      //   console.log(element);
-      //   console.log("nothing to replace here sir");
-      //   outputArray.push(element.join());
-      // }
-//     };
-//   });
-
-//   console.log(processArray);
-//   console.log(outputArray);
-
-// }
 
 window.addEventListener("load", function() {
   const form = document.getElementById("RobogerInput");
